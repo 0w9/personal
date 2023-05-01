a5e0d2fa1ef4d00a6f17090c42a0afd759665067
Lennard Dorst <lennardsolana@gmail.com>
2023-05-01 22:58:04 +0200

    Update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,5 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
+import '@/styles/globals.css'
 
-export default function App(props) {
-  const { Component, pageProps } = props;
-
-  return (
-    <>
-      <Head>
-        <title>Page title</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
