22b9274d7357f3f7bbbc91ff58ed9cc2aa3fbb5c
Lennard Dorst <lennardsolana@gmail.com>
2023-05-04 17:33:58 +0200

    Update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,12 @@
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import { Analytics } from '@vercel/analytics/react';
+ 
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
+ 
+export default MyApp;
