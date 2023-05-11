f8070bb28f798c43b04ae103eb6480fb3ca6348f
Lennard Dorst <lennardsolana@gmail.com>
2023-05-11 18:58:13 +0200

    Updates.

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,15 @@ function App() {
         <span className="decoration-double text-3xls">Lennard</span>
       </div>
 
+      <br />
       <div class="divide-y divide-blue-600" />
+      <br />
 
       <p>Hey! I&apos;m Lennard, a passionate software builder born in 2005. I mainly work on full-stack apps using NextJS/ React, but I am also experimenting with custom keyboards (firmware, PCB design) and design. To keep this intro short, you can find more details <span className="text-blue-600 hover:underline"><Link href={"/"}>here</Link></span>.</p>
 
+      <br />
       <div class="divide-y divide-blue-600" />
+      <br />
 
       <div>
         <p className="underline-offset-5 text-xl underline">Some projects I built:</p>
@@ -29,7 +33,9 @@ function App() {
         </div>
       </div>
 
+      <br />
       <div class="divide-y divide-blue-600" />
+      <br />
 
       <div>
         <p className="underline-offset-5 text-xl underline">Stuff I&apos;m hacking on:</p>
@@ -37,16 +43,22 @@ function App() {
         <div>
           I currently deliver <a className="text-blue-600">contracting work</a> to many clients around the world, mainly <a className="text-blue-600">providing full-stack and custom AI solutions</a>.
 
+          <br />
           <div class="divide-y divide-blue-600" />
+          <br />
 
           Apart from this I also work on different projects, like <a className="text-blue-600">automating agency workflows with AI and Zapier</a> or creating a <a className="text-blue-600">tool to manage accounting reports for small businesses</a>. All of those are collaborations with other teams or companies, but I also work with students on a <a className="text-blue-600">next-gen accountability tool for friends</a>, which we try to market with TikTok.
 
+          <br />
           <div class="divide-y divide-blue-600" />
+          <br />
 
 
           In my freetime I also hack on a <a className="text-blue-600">custom keyboard</a>, where I play with QMK and also am creating my first own PCB. This is a topic, like <a className="text-blue-600">embedded engineering</a>, that I would like to explore.
 
+          <br />
           <div class="divide-y divide-blue-600" />
+          <br />
 
           Other things I work on are <a className="text-blue-600">learning UI/UX design</a> and hitting a new record <a className="text-blue-600">biking min. 100km/week</a> or just building stuff in communities like <a className="text-blue-600">f.inc</a>,  <a className="text-blue-600">NexGen</a> and  <a className="text-blue-600">Buildspace</a>.
         </div>
