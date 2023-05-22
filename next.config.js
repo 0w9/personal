3393f54ac601053efd70e7cc4f60858d897321e5
Lennard Dorst <lennardsolana@gmail.com>
2023-05-22 16:59:14 +0200

    Multi-subdomains.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async rewrites() {
+    return {
+      beforeFiles: [
+          // if the host is `app.acme.com`,
+          // this rewrite will be applied
+          {
+              source: '/:path*',
+              has: [
+                  {
+                      type: 'host',
+                      value: 'beep.personal-7q7in50we-0w9.vercel.app',
+                  },
+              ],
+              destination: '/app/:path*',
+          },
+      ]
+  }
+  },
 }
 
 module.exports = nextConfig
---
+++ src/pages/info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+function App() {
+  return (
+    <div className="mt-10 max-w-3xl mx-auto px-10">
+      info :)
+    </div>
+  );
+}
+
+export default App;
