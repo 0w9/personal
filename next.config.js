b27c4de737963c0046656d8a97355752948a55b9
Lennard Dorst <lennardsolana@gmail.com>
2023-05-22 16:45:17 +0200

    Removing Krabs.

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  async rewrites() {
-    return [
-      {
-        source: '/:subdomain/:path*',
-        destination: 'https://:subdomain.localhost:3001/:path*',
-      },
-    ];
-  },
 }
 
 module.exports = nextConfig
