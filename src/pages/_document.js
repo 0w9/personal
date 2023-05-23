15f8ebb590a3e87eb273dff924269b8a151df576
Lennard Dorst <lennardsolana@gmail.com>
2023-05-23 16:37:29 +0200

    New font for /main

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,6 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Lora:ital@1&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,200;0,400;0,600;0,800;0,900;1,700;1,800;1,900&display=swap" rel='stylesheet' />
         </Head>
         <body>
           <Main />
